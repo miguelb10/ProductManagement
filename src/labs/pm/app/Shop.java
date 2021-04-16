317f9be5758b638adeaedc8f9be95da9996aee45
miguelb10 <miguel.blas_03@hotmail.com>
2021-04-15 22:06:30 -0500

    first commit

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2021 MIGUE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Locale;
+import labs.pm.data.Drink;
+import labs.pm.data.Food;
+import labs.pm.data.Product;
+import labs.pm.data.ProductManager;
+import labs.pm.data.Rating;
+import static labs.pm.data.Rating.*;
+
+/**
+ *
+ * @author MIGUE
+ */
+public class Shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ProductManager pm = new ProductManager(Locale.UK);
+        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), NOT_RATED);
+        pm.printProductReport();
+        p1 = pm.reviewProduct(p1, FOUR_START, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, TWO_START, "Rather weak tea");
+        p1 = pm.reviewProduct(p1, FOUR_START, "Fine tea");
+        p1 = pm.reviewProduct(p1, FOUR_START, "Good tea");
+        p1 = pm.reviewProduct(p1, FIVE_START, "Perfect tea");
+        p1 = pm.reviewProduct(p1, THREE_START, "Just add some lemon");
+        pm.printProductReport();
+//        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), FOUR_START);
+//        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), FIVE_START, LocalDate.now().plusDays(2));
+//        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), TWO_START, LocalDate.now());
+//        Product p5 = p3.applyRating(THREE_START);
+//        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_START);
+//        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_START, LocalDate.now().plusDays(2));
+//        Product p8 = p4.applyRating(FIVE_START);
+//        Product p9 = p1.applyRating(TWO_START);
+//        System.out.println(p6.equals(p7));
+//        System.out.println(p1.getBestBefore());
+//        System.out.println(p3.getBestBefore());
+//        if (p3 instanceof Food) {
+//            LocalDate bestBefore = ((Food) p3).getBestBefore();
+//        }
+//        p1.setId(101);
+//        p1.setName("Tea");
+//        p1.setPrice(BigDecimal.valueOf(1.99));
+//        p3 = p3.applyRating(THREE_START);
+//        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount() + " " + p1.getRating().getStars());
+//        System.out.println(p2.getId() + " " + p2.getName() + " " + p2.getPrice() + " " + p2.getDiscount() + " " + p2.getRating().getStars());
+//        System.out.println(p3.getId() + " " + p3.getName() + " " + p3.getPrice() + " " + p3.getDiscount() + " " + p3.getRating().getStars());
+//        System.out.println(p4.getId() + " " + p4.getName() + " " + p4.getPrice() + " " + p4.getDiscount() + " " + p4.getRating().getStars());
+//        System.out.println(p5.getId() + " " + p5.getName() + " " + p5.getPrice() + " " + p5.getDiscount() + " " + p5.getRating().getStars());
+//        System.out.println(p1);
+//        System.out.println(p2);
+//        System.out.println(p3);
+//        System.out.println(p4);
+//        System.out.println(p5);
+//        System.out.println(p6);
+//        System.out.println(p7);
+//        System.out.println(p8);
+//        System.out.println(p9);
+    }
+
+}
---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2021 MIGUE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
+import java.time.LocalDate;
+import java.util.Objects;
+import static labs.pm.data.Rating.*;
+
+/**
+ *
+ * @author MIGUE
+ */
+public abstract class Product implements Rateable<Product> {
+
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private Rating rating;
+
+//    Product() {
+//        this(0, "no name", BigDecimal.ZERO);
+//    }
+    Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+//    public void setPrice(final BigDecimal price) {
+//        //price = BigDecimal.ONE;
+//        this.price = price;
+//    }
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    @Override
+    public Rating getRating() {
+        return rating;
+    }
+
+//    public abstract Product applyRating(Rating newRating);
+//    {
+//        return new Product(this.id, this.name, this.price, newRating);
+//    }
+    /**
+     * Get the value of the best before for the product
+     *
+     * @return the value of bestBefore
+     */
+    public LocalDate getBestBefore() {
+        return LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + price + ", " + getDiscount() + ", " + rating.getStars() + " " + getBestBefore();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+//        if (obj != null && getClass() == obj.getClass()) {
+        if (obj instanceof Product) {
+            final Product other = (Product) obj;
+            return this.id == other.id && Objects.equals(this.name, other.name);
+        }
+        return false;
+    }
+
+}
