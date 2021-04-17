2b0f27c5d37478774afa0c0ea23f51f73e3ce072
miguelb10 <miguel.blas_03@hotmail.com>
2021-04-16 19:44:49 -0500

    class collections

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -38,14 +38,44 @@ public class Shop {
     public static void main(String[] args) {
         ProductManager pm = new ProductManager(Locale.UK);
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), NOT_RATED);
-        pm.printProductReport();
+        pm.printProductReport(p1);
         p1 = pm.reviewProduct(p1, FOUR_START, "Nice hot cup of tea");
         p1 = pm.reviewProduct(p1, TWO_START, "Rather weak tea");
         p1 = pm.reviewProduct(p1, FOUR_START, "Fine tea");
         p1 = pm.reviewProduct(p1, FOUR_START, "Good tea");
         p1 = pm.reviewProduct(p1, FIVE_START, "Perfect tea");
         p1 = pm.reviewProduct(p1, THREE_START, "Just add some lemon");
-        pm.printProductReport();
+        pm.printProductReport(p1);
+
+        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), NOT_RATED);
+        p2 = pm.reviewProduct(p2, THREE_START, "Coffee was ok");
+        p2 = pm.reviewProduct(p2, ONE_START, "where is the milk?!");
+        p2 = pm.reviewProduct(p2, FIVE_START, "It's perfect with ten spoons of sugar!");
+        pm.printProductReport(p2);
+
+        Product p3 = pm.createProduct(103, "Coffee", BigDecimal.valueOf(1.99), NOT_RATED, LocalDate.now().plusDays(2));
+        p3 = pm.reviewProduct(p3, THREE_START, "Coffee was ok");
+        p3 = pm.reviewProduct(p3, ONE_START, "where is the milk?!");
+        p3 = pm.reviewProduct(p3, FIVE_START, "It's perfect with ten spoons of sugar!");
+        pm.printProductReport(p3);
+
+        Product p4 = pm.createProduct(104, "Coffee", BigDecimal.valueOf(1.99), NOT_RATED, LocalDate.now());
+        p4 = pm.reviewProduct(p4, THREE_START, "Coffee was ok");
+        p4 = pm.reviewProduct(p4, ONE_START, "where is the milk?!");
+        p4 = pm.reviewProduct(p4, FIVE_START, "It's perfect with ten spoons of sugar!");
+        pm.printProductReport(p4);
+
+        Product p5 = pm.createProduct(105, "Coffee", BigDecimal.valueOf(1.99), NOT_RATED);
+        p5 = pm.reviewProduct(p5, THREE_START, "Coffee was ok");
+        p5 = pm.reviewProduct(p5, ONE_START, "where is the milk?!");
+        p5 = pm.reviewProduct(p5, FIVE_START, "It's perfect with ten spoons of sugar!");
+        pm.printProductReport(p5);
+
+        Product p6 = pm.createProduct(106, "Coffee", BigDecimal.valueOf(1.99), NOT_RATED);
+        p6 = pm.reviewProduct(p6, THREE_START, "Coffee was ok");
+        p6 = pm.reviewProduct(p6, ONE_START, "where is the milk?!");
+        p6 = pm.reviewProduct(p6, FIVE_START, "It's perfect with ten spoons of sugar!");
+        pm.printProductReport(p6);
 //        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), FOUR_START);
 //        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), FIVE_START, LocalDate.now().plusDays(2));
 //        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), TWO_START, LocalDate.now());
